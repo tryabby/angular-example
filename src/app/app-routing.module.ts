b8359a3f3de102d639de309ae229bee6475f5678
trishouser <mail@trishouser.de>
2023-05-17 11:26:42 +0200

    add tailwind and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MarketingComponent } from './marketing/marketing.component';
+import { MainComponent } from './main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [  { 
+  path: 'home', component: HomeComponent },
+  {path: 'marketing', component: MarketingComponent },
+  {path: '', component: MainComponent}
+,];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MarketingComponent } from './marketing/marketing.component';
+import { MainComponent } from './main/main.component';
+// import { AbbyModule } from '@tryabby/angular'W
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MarketingComponent,
+    MainComponent, 
+    // AbbyModule.forRoot(abby)
   ],
   imports: [
     BrowserModule,
